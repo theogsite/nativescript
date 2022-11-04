549de4ab172cd63948941f742489f8028dd727f0
theogsite <98266162+theogsite@users.noreply.github.com>
2022-11-05 04:07:19 +0530

    Initial commit

---
+++ app/components/Home.svelte
@@ -0,0 +1,16 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <label class="text-3xl align-middle text-center text-gray-500" text="{message}" />
+    </gridLayout>
+</page>
+
+<script lang="ts">
+    let message: string = "Analytics App"
+</script>
+
+<style>
+    /* .info {
+        font-size: 20;
+    } */
+</style>
